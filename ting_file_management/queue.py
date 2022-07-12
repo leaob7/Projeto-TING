4fd148cf730868beae2f79fcffebc646e7b2e671
Guilherme Leao <leaob7@gmail.com>
2022-07-12 15:22:28 -0300

    feat: implementa class Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,16 +1,28 @@
+from queue import Empty
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
-
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self) == 0:
+            return None
+
+        value = self._data[0]
+        del self._data[0]
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > len(self):
+            raise IndexError('Invalid index')
+
+        value = self._data[index]
+        return value
+
