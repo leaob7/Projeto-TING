0a6a7e03371f0bfa113e58c96ed560640b960f05
Guilherme Leao <leaob7@gmail.com>
2022-07-12 15:57:12 -0300

    feat: implementa txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        file_type = path_file.split('.')[1]
+
+        if file_type != 'txt':
+            print("Formato inválido", file=sys.stderr)
+
+        reader = open(path_file, 'r').read().splitlines()
+
+        return reader
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
