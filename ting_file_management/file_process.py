eb0c806b4f4f3a1a877a676e21cd4d671916228e
Guilherme Leao <leaob7@gmail.com>
2022-07-13 13:48:53 -0300

    feat: implementa process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance._data:
+        if item["nome_do_arquivo"] == path_file:
+            return None
+
+    read = txt_importer(path_file)
+
+    content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(read),
+        "linhas_do_arquivo": read
+        } 
+
+    instance.enqueue(content)
+
+    print(str(content), file=sys.stdout)
 
 
 def remove(instance):
