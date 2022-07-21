77ed346fc1f6da1fe47e532b64c2162e716f913f
Guilherme Leao <leaob7@gmail.com>
2022-07-21 15:05:04 -0300

    feat: implementa search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -14,8 +14,28 @@ def exists_word(word, instance):
             "arquivo": file_name,
             "ocorrencias": lines
         })
+
     return return_data
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return_data = []
+    lines = []
+    file_name = instance._data[0]["nome_do_arquivo"]
+    file_lines = instance._data[0]["linhas_do_arquivo"]
+
+    for line in file_lines:
+        if word.lower() in line.lower():
+            lines.append({
+                "linha": file_lines.index(line) + 1,
+                "conteudo": line
+                })
+
+    if len(lines) > 0:
+        return_data.append({
+            "palavra": word,
+            "arquivo": file_name,
+            "ocorrencias": lines
+        })
+
+    return return_data
